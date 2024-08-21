0275dd681ec57b168827e2ff0ae8261f0ab28630
Tom Monks <monksy@gmail.com>
2024-08-21 09:36:12 -0400

    Remove default PageController

--- lib/mpg_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule MPGWeb.PageController do
-  use MPGWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
