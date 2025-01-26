db94812eb4ef682e8bff9c7044765620be8750c6
Tom Monks <monksy@gmail.com>
2025-01-26 10:42:58 -0500

    Fix New Quiz link

--- lib/mpg_web/live/quiz_live.ex
+++ lib/mpg_web/live/quiz_live.ex
@@ -228,7 +228,7 @@ defmodule MPGWeb.QuizLive do
         <.link
           id="new-quiz-button"
           class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
-          patch={~p"/quiz/new_quiz"}
+          patch={~p"/quiz/#{@server_id}/new_quiz"}
         >
           New Quiz
         </.link>
