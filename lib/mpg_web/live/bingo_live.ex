c43ff3d7398137e5124cb59fb46806766751015b
Tom Monks <monksy@gmail.com>
2025-05-04 11:22:35 -0400

    Bingo player join functionality

--- lib/mpg_web/live/bingo_live.ex
+++ lib/mpg_web/live/bingo_live.ex
@@ -1,18 +1,30 @@
 defmodule MPGWeb.BingoLive do
   use MPGWeb, :live_view
+  use Phoenix.Component
 
   alias MPG.Bingos.Session
+  alias MPG.Bingos.Player
+  alias Phoenix.PubSub
 
   @impl true
-  def mount(%{"id" => server_id}, _session, socket) do
+  def mount(%{"id" => server_id}, session, socket) do
+    %{"session_id" => session_id} = session
+
+    socket =
+      socket
+      |> assign(:page_title, "Dinner Bingo")
+      |> assign(:primary_color, "bg-orange-500")
+      |> assign(:session_id, session_id)
+
     case Session.get_state(server_id) do
       {:ok, state} ->
+        :ok = PubSub.subscribe(MPG.PubSub, server_id)
+
         {:ok,
          socket
-         |> assign(:page_title, "Dinner Bingo")
-         |> assign(:primary_color, "bg-orange-500")
          |> assign(:server_id, server_id)
-         |> assign(:state, state)}
+         |> assign(:state, state)
+         |> assign_player()}
 
       {:error, :not_found} ->
         {:ok,
@@ -23,7 +35,9 @@ defmodule MPGWeb.BingoLive do
   end
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
+    %{"session_id" => _session_id} = session
+
     # generate a random 5 digit server ID
     server_id = Enum.random(10000..99999) |> Integer.to_string()
 
@@ -33,15 +47,76 @@ defmodule MPGWeb.BingoLive do
     {:ok, push_navigate(socket, to: "/bingo/#{server_id}")}
   end
 
+  defp assign_player(%{assigns: assigns} = socket) do
+    player = Enum.find(assigns.state.players, &(&1.id == assigns.session_id))
+    assign(socket, :player, player)
+  end
+
+  @impl true
+  def handle_event("join", %{"player_name" => player_name}, socket) do
+    %{session_id: session_id, server_id: server_id} = socket.assigns
+    Session.add_player(server_id, session_id, player_name)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:state_updated, state}, socket) do
+    {:noreply,
+     socket
+     |> assign(:state, state)
+     |> assign_player()}
+  end
+
   @impl true
   def render(assigns) do
     ~H"""
     <div class="flex flex-col items-center p-4">
       <h1 class="text-2xl font-bold mb-4"><%= @page_title %></h1>
       <div class="w-full max-w-md">
-        <p>Welcome to Bingo!</p>
-        <p>Server ID: <%= @server_id %></p>
+        <%= if @player == nil do %>
+          <form id="join-form" phx-submit="join">
+            <div class="flex gap-4 pt-16">
+              <div>
+                <input
+                  type="text"
+                  name="player_name"
+                  placeholder="Enter your name"
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+              </div>
+              <div>
+                <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        <% else %>
+          <div class="mb-8">
+            <div id="player-list" class="flex gap-2">
+              <%= for player <- @state.players do %>
+                <.player_avatar player={player} />
+              <% end %>
+            </div>
+          </div>
+        <% end %>
       </div>
+    </div>
+    """
+  end
+
+  attr :player, Player, required: true
+  attr :size, :integer, default: 12
+
+  defp player_avatar(assigns) do
+    ~H"""
+    <div
+      class={"relative flex items-center justify-center w-#{@size} h-#{@size} text-white font-bold rounded-full"}
+      data-role="avatar"
+      style={"background-color: #{@player.color}"}
+      id={"player-" <> @player.id}
+    >
+      <%= String.slice(@player.name, 0..2) %>
     </div>
     """
   end
--- test/mpg_web/live/bingo_live_test.exs
+++ test/mpg_web/live/bingo_live_test.exs
@@ -8,7 +8,15 @@ defmodule MPGWeb.BingoLiveTest do
   setup do
     server_id = "test_server"
     start_supervised!({Session, [name: server_id]})
-    %{server_id: server_id}
+
+    # populate a session_id on the conn
+    session_id = UUID.uuid4()
+    conn = init_test_session(build_conn(), %{session_id: session_id})
+
+    # subscribe to PubSub
+    :ok = Phoenix.PubSub.subscribe(MPG.PubSub, server_id)
+
+    %{conn: conn, server_id: server_id, session_id: session_id}
   end
 
   test "visiting /bingo redirects to a random server ID", %{conn: conn} do
@@ -25,15 +33,43 @@ defmodule MPGWeb.BingoLiveTest do
 
   test "redirects to home page if the server ID is invalid", %{conn: conn} do
     {:error, {:live_redirect, %{to: new_path, flash: flash}}} =
-      live(conn, ~p"/quiz/invalidid")
+      live(conn, ~p"/bingo/invalidid")
 
     assert new_path == "/"
     assert flash["error"] == "Game not found"
   end
 
-  test "can access bingo page with valid server ID", %{conn: conn, server_id: server_id} do
-    {:ok, _view, html} = live(conn, "/bingo/#{server_id}")
-    assert html =~ "Dinner Bingo"
-    assert html =~ server_id
+  test "if the player with the session_id does not exist, prompts for name", %{
+    conn: conn,
+    server_id: server_id
+  } do
+    {:ok, view, _html} = live(conn, ~p"/bingo/#{server_id}")
+
+    assert has_element?(view, "#join-form")
+  end
+
+  test "can join the game", %{conn: conn, server_id: server_id, session_id: session_id} do
+    {:ok, view, _html} = live(conn, ~p"/bingo/#{server_id}")
+
+    view
+    |> form("#join-form", %{player_name: "Peter"})
+    |> render_submit()
+
+    assert_receive {:state_updated, %State{}}
+    assert has_element?(view, "#player-#{session_id}[data-role=avatar]", "Pet")
+    refute has_element?(view, "#join-form")
+  end
+
+  test "loads user from session_id if it exists", %{
+    conn: conn,
+    server_id: server_id,
+    session_id: session_id
+  } do
+    Session.add_player(server_id, session_id, "Peter")
+
+    {:ok, view, _html} = live(conn, ~p"/bingo/#{server_id}")
+
+    assert has_element?(view, "#player-#{session_id}[data-role=avatar]", "Pet")
+    refute has_element?(view, "#join-form")
   end
 end
